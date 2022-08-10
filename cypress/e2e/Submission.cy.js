986884d48b6578c20f4088114141da872ad715e2
Spelanky <jahmadharma@gmail.com>
2022-08-10 21:08:58 +0300

    Update Submission.cy.js

--- cypress/e2e/Submission.cy.js
+++ cypress/e2e/Submission.cy.js
@@ -1,10 +1,11 @@
 /// <reference types="Cypress" />
 import Form from './Page Objects/Page Object.js';
+// import Random from './Page Objects/Page Object.js';
 
 //Set variables for test
 const base_url = 'https://spelanky.github.io/spelanky2.github.io/';
 const name = 'Mitka';
-const email = 'mymail@test.com';
+const email = 'mymail';
 const message = 'Lorem Ipsum Message Text';
 const birthday = '1985-04-04';
 
@@ -23,24 +24,24 @@ function Random() {
 describe('Send submission', () => {
    context('Submission', () => {
       const sent = new Form();
+      // const random = new Random();
 
-      it('Sent', () => {
+      it('Sent Sumbission', () => {
          cy.visit(`${base_url}`);
          cy.viewport(1600, 1200);
          cy.location('protocol').should('eq', 'https:');
 
          sent.Name().type(`${name}`);
+
          // sent.Email().type(`${email}`);
-
-         sent.Email().type(`mail_${Random()} @test.com`);
-
+         sent.Email().type(`${email}_${Random()}@test.com`);
          sent.Message().type(`${message}`);
 
          // Attach file
          sent.Attachment().selectFile('file1.png');
-
          //Attach 3 files
          sent.Attachment2().selectFile(['file1.png', 'file2.png', 'file3.png']);
+
          //Date field
          sent.Birthday().type(`${birthday}`);
          //Radio button check
