e4f019e2bb7bb81f9e9e9a768eb00eea69828702
Spelanky <jahmadharma@gmail.com>
2022-08-11 13:08:39 +0300

    gf

--- cypress/e2e/Page Objects/Page Object.js
+++ cypress/e2e/Page Objects/Page Object.js
@@ -28,5 +28,16 @@ class Form {
       return cy.get("button[type='submit']");
    }
 }
+function Random() {
+   let text = '';
+   let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+   for (
+      var i = 0;
+      i < 1;
+      i++ //How many chars
+   )
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+   return text;
+}
 
-export default Form;
+export default { Form, Random };
